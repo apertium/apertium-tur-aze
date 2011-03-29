d248b3a7974706dbf674191f35c6e241133d98ce
Francis M. Tyers <ftyers@prompsit.com>
2011-03-29 21:48:37 +0000

    more delicious words

--- tr-az.sh
+++ tr-az.sh
@@ -1,4 +1,6 @@
 sed 's/ /\n/g' |\
 hfst-lookup -O apertium $TRMORPH 2> /dev/null |\
 apertium-tagger -g tr-az.prob |\
-lt-proc -b tr-az.autobil.bin
+apertium-transfer apertium-tr-az.tr-az.t1x tr-az.t1x.bin tr-az.autobil.bin
+
+#lt-proc -b tr-az.autobil.bin
