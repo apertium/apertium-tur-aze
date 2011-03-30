a7dff9e2f0577ff4253faf5f6a49435d5965f481
Francis M. Tyers <ftyers@prompsit.com>
2011-03-30 11:52:11 +0000

    update script to use hfst-proc

--- tr-az.sh
+++ tr-az.sh
@@ -1,5 +1,4 @@
-sed 's/ /\n/g' |\
-hfst-lookup -O apertium $TRMORPH 2> /dev/null |\
+hfst-proc $TRMORPH |\
 apertium-tagger -g tr-az.prob |\
 apertium-transfer apertium-tr-az.tr-az.t1x tr-az.t1x.bin tr-az.autobil.bin
 
