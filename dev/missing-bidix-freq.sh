5900ba0ab13f60815efc5473b6f1e978a6d4353e
Francis M. Tyers <ftyers@prompsit.com>
2011-03-31 18:31:24 +0000

    some proper nouns

--- dev/missing-bidix-freq.sh
+++ dev/missing-bidix-freq.sh
@@ -7,7 +7,7 @@ SELECT (inf);" > /tmp/temp_cg ;
 
 cg-comp /tmp/temp_cg /tmp/temp_cg.bin ;
 
-cat $INPUT | grep '<v>' | grep '@' | cut -f2 -d'@' | cut -f1 -d'<' | lt-proc -w $DEV/../../tr-az.automorf.bin  | cg-proc /tmp/temp_cg.bin  | cut -f2- -d'/' | sed 's/<pres><p3><sg>//g' | grep '<vblex>' | cut -f1 -d'$' | sort -f | uniq -c | sort -gr  | grep -v '[0-9] $' > $DEV/pending_verbs.txt
+cat $INPUT | grep '<v>' | grep '@' | cut -f2 -d'@' | cut -f1 -d'<' | lt-proc -w $DEV/../../tr-az.automorf.bin  | cg-proc /tmp/temp_cg.bin  | cut -f2- -d'/' | sed 's/<pres><p3><sg>//g' | grep '<v>' | cut -f1 -d'$' | sort -f | uniq -c | sort -gr  | grep -v '[0-9] $' > $DEV/pending_verbs.txt
 
 cat $INPUT | grep '@' | grep '<adv>' | grep -v '<v' | sh ~/scripts/lowercase.sh  | sort -f | sed 's/^\W*\^/^/g' | sort -f | uniq -c | sort -gr  | grep -v '[0-9] $' > $DEV/pending_adverbs.txt
 
@@ -21,7 +21,7 @@ cat $INPUT | grep '@' | grep '<prn>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc
 
 cat $INPUT | grep '@' | grep '<num>'  | sed 's/<sg>//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_numerals.txt
 
-cat $INPUT | grep '@' | grep '<np>'  | sed 's/<sg>//g' | sed 's/<pl>//g' | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_proper_nouns.txt
+cat $INPUT | grep '@' | grep '<np>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<nom>\|<abs>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p\)//g' | sed 's/<pl>//g' | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_proper_nouns.txt
 
 cat $INPUT | grep '@' | grep '<cnj'  | sed 's/<sg>//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_conjunctions.txt
 
