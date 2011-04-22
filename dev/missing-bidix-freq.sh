d62fc8b6cfb9bea1e79bc8f9543d52bd06dd79d2
Francis M. Tyers <ftyers@prompsit.com>
2011-04-22 16:19:11 +0000

    more

--- dev/missing-bidix-freq.sh
+++ dev/missing-bidix-freq.sh
@@ -17,13 +17,13 @@ cat $INPUT | grep '@' | grep '<post' | grep -v '<v' | sh ~/scripts/lowercase.sh 
 
 cat $INPUT | grep '@' | grep '<adj>'  | sed 's/<attr>//g' | sed 's/<pred>//g' | sh ~/scripts/lowercase.sh  | sed 's/[ανρχί]/\n/g' | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' | grep '<adj>' | grep '@' > $DEV/pending_adjectives.txt
 
-cat $INPUT | grep '@' | grep '<n>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<nom>\|<abs>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p\)//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $'  > $DEV/pending_nouns.txt
+cat $INPUT | grep '@' | grep '<n>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<dir>\|<nom>\|<abl>\|<abs>\|<1s>\|<2s>\|<3s>\|<1p>\|<2p>\|<3p>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p>\)//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $'  > $DEV/pending_nouns.txt
 
-cat $INPUT | grep '@' | grep '<prn>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<nom>\|<abs>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p\)//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $'  > $DEV/pending_pronouns.txt
+cat $INPUT | grep '@' | grep '<prn>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<dir>\|<nom>\|<abl>\|<abs>\|<1s>\|<2s>\|<3s>\|<1p>\|<2p>\|<3p>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p>\)//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $'  > $DEV/pending_pronouns.txt
 
 cat $INPUT | grep '@' | grep '<num>'  | sed 's/<sg>//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh  | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_numerals.txt
 
-cat $INPUT | grep '@' | grep '<np>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<nom>\|<abs>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p\)//g' | sed 's/<pl>//g' | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_proper_nouns.txt
+cat $INPUT | grep '@' | grep '<np>'  | sed 's/\(<sg>\|<ins>\|<acc>\|<dat>\|<loc>\|<gen>\|<dir>\|<nom>\|<abl>\|<abs>\|<1s>\|<2s>\|<3s>\|<1p>\|<2p>\|<3p>\|<p1s>\|<p2s>\|<p3s>\|<p1p>\|<p2p>\|<p3p>\)//g' | sed 's/<pl>//g' | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_proper_nouns.txt
 
 cat $INPUT | grep '@' | grep '<cnj'  | sed 's/<sg>//g' | sed 's/<pl>//g' | sh ~/scripts/lowercase.sh | sed 's/^\W*\^/^/g' | sort -f | uniq -c  | sort -gr  | grep -v '[0-9] $' > $DEV/pending_conjunctions.txt
 
