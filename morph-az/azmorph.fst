f4bc4f9ddc75f76b4fb6c46c016fea299f131777
Francis M. Tyers <ftyers@prompsit.com>
2011-08-09 14:51:40 +0000

    update pronouns

--- morph-az/azmorph.fst
+++ morph-az/azmorph.fst
@@ -648,33 +648,20 @@ ALPHABET = [#Asym#] <>:[#LEsymS#] \
            <>:[#TMP#] <>:<RB> <>:<MB>
 
 
-$afilter$ = .*
-
-$afilter$ || <BoW> $WORD$ <EoW> || "<phon/phon.a>"
-
+$deltempsym$ = .
 
 %
 % filter/rearrange symbols in the analysis layer
 %
 
-
-ALPHABET = [#Ssym##deriv##infl#] \
-           <>:[#LEsymS#] \
-           ç:<c> p:<p> t:<t> k:<k> g:<g> k:<K> t:<D> ç:<C> \
-           <A><I><Q> e:<e> \
-           a:<pA> ı:<pI> o:<pO> u:<pU> \
-           <>:[#TMP#] <>:<RB> <>:<MB>
-
-$deltempsym$ = .
-
 ALPHABET = [#pos##subcat#]
 
 #=SC# = #subcat#
 #=C# = #pos#
+
 $swap$ = ({[#=C#][#=SC#]}:{[#=SC#][#=C#]} | [#pos#])
 
 
 $afilter$ = ($deltempsym$  | $swap$)*
 
 $afilter$ || <BoW> $WORD$ <EoW> || "<phon/phon.a>"
-
--- update-azmorph.py
+++ update-azmorph.py
@@ -13,26 +13,26 @@ sys.stdout = codecs.getwriter('utf-8')(sys.stdout);
 sys.stderr = codecs.getwriter('utf-8')(sys.stderr);
 
 tag_files = {
-	'adjectives.dix': '<adj>',
-	'adverbs.dix': '<adv>',
-	'cnjadv.dix': '<cnjadv>',
-	'cnjcoo.dix': '<cnjcoo>',
-	'cnjsub.dix': '<cnjsub>',
-	'det.dix': '<det>',
-	'interjections.dix': '<ij>',
-	'nouns.dix': '<n>',
-	'pn_acr.dix': '<np><acr>',
-	'pn_ant.dix': '<np><ant>',
-	'pn_cog.dix': '<np><cog>',
-	'pn_org.dix': '<np><org>',
-	'postpositions.dix': '<postp>',
-	'postpositions_infl.dix': '<postp>',
-	'pronouns.dix': '<prn>',
-	'proper_nouns.dix': '<np>',
-	'toponyms.dix': '<np><top>',
-	'verbs.dix': '<v>',
-	'verbs_iv.dix': '<v><iv>',
-	'verbs_tv.dix': '<v><tv>'
+	'adjectives.dix': ['<adj>'],
+	'adverbs.dix': ['<adv>'],
+	'cnjadv.dix': ['<cnjadv>'],
+	'cnjcoo.dix': ['<cnjcoo>'],
+	'cnjsub.dix': ['<cnjsub>'],
+	'det.dix': ['<det>'],
+	'interjections.dix': ['<ij>'],
+	'nouns.dix': ['<n>'],
+	'pn_acr.dix': ['<np><acr>'],
+	'pn_ant.dix': ['<np><ant>'],
+	'pn_cog.dix': ['<np><cog>'],
+	'pn_org.dix': ['<np><org>'],
+	'postpositions.dix': ['<postp>'],
+	'postpositions_infl.dix': ['<postp>'],
+	'pronouns.dix': ['<prn>', '<prn><locp>', '<prn><dem>', '<prn><pers>', '<prn><qst>', '<prn><ind>'],
+	'proper_nouns.dix': ['<np>'],
+	'toponyms.dix': ['<np><top>'],
+	'verbs.dix': ['<v>'],
+	'verbs_iv.dix': ['<v><iv>'],
+	'verbs_tv.dix': ['<v><tv>']
 };
 
 tags_stems = {};
@@ -81,42 +81,52 @@ for f in tag_files.keys(): #{
 	print '\n*' , f.decode('utf-8') + ':';
 	# Read lexicon file and check to see if there is an entry for each line in the bidix	
 
-	if tag_files[f] not in tags_stems: #{
-		continue;
-	#}
-
-	if not os.path.exists(azmorph + '/lexicon/' + f): #{
-		continue
-	#}
 	if f.count('.dix') > 0: #{
 		outfile = file(azout + '/lexicon/' + f.replace('.dix', ''), 'w+');
 	else: #{
 		outfile = file(azout + '/lexicon/' + f, 'w+');
 	#}
-	for line in file(azmorph + '/lexicon/' + f).read().split('\n'): #{
-		if line == '': #{
+	outfile.close();
+
+	for tag in tag_files[f]: #{
+	
+		if tag not in tags_stems: #{
 			continue;
 		#}
-		lema = '';
-		if line.count('#') > 0: #{
-			lema = line.split('#')[1].strip();
-		elif line.count('<e lm="') > 0: #{
-			lema = line.split('lm="')[1].split('"')[0].strip();
-		else: #{
-			lema = line.strip();
+	
+		if not os.path.exists(azmorph + '/lexicon/' + f): #{
+			continue
 		#}
-		if lema in tags_stems[tag_files[f]]: #{
-			print '+' , f.decode('utf-8') , lema.decode('utf-8') , line.decode('utf-8');
-			if line.count('\t') > 0: #{
-				outfile.write(line.split('\t')[0] + '\n');
-			elif line.count('<i>') > 0: #{
-				outfile.write(line.split('<i>')[1].split('</i>')[0].replace('<s n="', '<').replace('"/>', '>').replace('<b/>', ' ') + '\n');
-			else: #{
-				outfile.write(line + '\n');
+		if f.count('.dix') > 0: #{
+			outfile = file(azout + '/lexicon/' + f.replace('.dix', ''), 'a+');
+		else: #{
+			outfile = file(azout + '/lexicon/' + f, 'a+');
+		#}
+		for line in file(azmorph + '/lexicon/' + f).read().split('\n'): #{
+			if line == '': #{
+				continue;
 			#}
-		else: #{
-			print '-' , f.decode('utf-8') , lema.decode('utf-8') , line.decode('utf-8');
+			lema = '';
+			if line.count('#') > 0: #{
+				lema = line.split('#')[1].strip();
+			elif line.count('<e lm="') > 0: #{
+				lema = line.split('lm="')[1].split('"')[0].strip();
+			else: #{
+				lema = line.strip();
+			#}
+			if lema in tags_stems[tag]: #{
+				print '+' , f.decode('utf-8') , lema.decode('utf-8') , line.decode('utf-8');
+				if line.count('\t') > 0: #{
+					outfile.write(line.split('\t')[0] + '\n');
+				elif line.count('<i>') > 0: #{
+					outfile.write(line.split('<i>')[1].split('</i>')[0].replace('<s n="', '<').replace('"/>', '>').replace('<b/>', ' ') + '\n');
+				else: #{
+					outfile.write(line + '\n');
+				#}
+			else: #{
+				print '-' , f.decode('utf-8') , lema.decode('utf-8') , line.decode('utf-8');
+			#}
 		#}
+		outfile.close();
 	#}
-	outfile.close();
 #}
