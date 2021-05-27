348e295fd4c79e563473fe4ddaf0617a35eeb0ff
Tino Didriksen <mail@tinodidriksen.com>
2021-05-27 12:13:30 +0200

    OFTC, http -> https, url fixups

--- pending-tests.sh
+++ pending-tests.sh
@@ -1,1 +1,1 @@
-aq-regtest -d . tr-az http://wiki.apertium.org/wiki/Special:Export/Turkish_and_Azerbaijani/Pending_tests -X dev/pending-stats.xml
+aq-regtest -d . tr-az https://wiki.apertium.org/wiki/Special:Export/Turkish_and_Azerbaijani/Pending_tests -X dev/pending-stats.xml
--- regression-tests.sh
+++ regression-tests.sh
@@ -1,1 +1,1 @@
-aq-regtest -d . tr-az http://wiki.apertium.org/wiki/Special:Export/Turkish_and_Azerbaijani/Regression_tests -X dev/regression-stats.xml
+aq-regtest -d . tr-az https://wiki.apertium.org/wiki/Special:Export/Turkish_and_Azerbaijani/Regression_tests -X dev/regression-stats.xml
